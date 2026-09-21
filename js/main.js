b1c74d2db7fd87130f52ac605566e534abb5a5da
nyxie bones <nyxiebones@hotmail.com>
2026-09-21 12:31:01 +0100

    active

--- js/main.js
+++ js/main.js
@@ -1,17 +1,26 @@
-const popupButtons = document.querySelectorAll('[data-popup]');
+const popupButtons = document.querySelectorAll('[data-popup');
+
 popupButtons.forEach((button) => {
     const popupId = button.dataset.popup;
     const popup = document.querySelector(`#${popupId}`);
     const closeButton = popup.querySelector('.popup-close');
 
     button.addEventListener('click', () => {
+
         document.querySelectorAll('.popup').forEach((popup) => {
             popup.hidden = true;
         });
+
+        document.querySelectorAll('.nav-item').forEach((button) => {
+            button.classList.remove('active');
+        });
+
         popup.hidden = false;
+        button.classList.add('active');
     });
 
     closeButton.addEventListener('click', () => {
         popup.hidden = true;
+        button.classList.remove('active');
     });
 });
